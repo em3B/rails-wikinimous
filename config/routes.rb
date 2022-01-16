7751b3f23861614f2fcba7d405e712f95bb9af3f
Emily Baird <emilym.baird@gmail.com>
2022-01-16 00:06:43 +0000

    url of titles fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'articles', to: 'articles#index'
   get 'articles/new', to: 'articles#new'
-  get 'articles/:id', to: 'articles#show'
+  get 'articles/:id', to: 'articles#show', as: :article
   post 'articles', to: 'articles#create'
   get 'articles/:id/edit', to: 'articles#edit'
   patch 'articles/:id', to: 'articles#update'
