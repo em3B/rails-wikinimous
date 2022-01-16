eb94d002d7231d8ec08e9b95c5edab3f10ee783e
Emily Baird <emilym.baird@gmail.com>
2022-01-16 08:18:37 +0000

    update method not working

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,7 +24,7 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     @article.update(article_params)
 
-    redirect_to article_path(@article)
+    redirect_to articles_path(@article)
   end
 
   def destroy
