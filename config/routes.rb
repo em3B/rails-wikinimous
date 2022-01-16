491b3974da1ebb63481ef682e4c7e5ba438e6140
Emily Baird <emilym.baird@gmail.com>
2022-01-16 07:32:28 +0000

    show page created, destroy method updated

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   get 'articles/new', to: 'articles#new'
   get 'articles/:id', to: 'articles#show', as: :article
   post 'articles', to: 'articles#create'
-  get 'articles/:id/edit', to: 'articles#edit'
+  get 'articles/:id/edit', to: 'articles#edit', as: :edit
   patch 'articles/:id', to: 'articles#update'
-  delete 'restaurants/:id', to: 'articles#destroy'
+  delete 'restaurants/:id', to: 'articles#destroy', as: :destroy
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
